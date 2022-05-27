4b2bbeaf55126f9ffa56a8a0fbfb77ae1b14f43d
parkjungkwan <pakjkwan@gmail.com>
2022-05-27 15:09:55 +0900

    15

---
+++ components/Layouts/Layout.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+import styles from "@/styles/Layout.module.css";
+import Header from './Header'
+import Footer from './Footer'
+
+interface Props {
+    children?: ReactNode
+}
+
+export function Layout({ children }: Props){
+  return (
+    <div className={styles.container}>
+      <Header/>
+        { children }
+      <Footer/>
+    </div>
+
+  );
+}
