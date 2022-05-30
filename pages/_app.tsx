6d317302bc1aaffedecd7d73e94abed811099227
parkjungkwan <pakjkwan@gmail.com>
2022-05-30 12:19:19 +0900

    12

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import { AppProps } from 'next/app';
-import '@/styles/globals.css';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
+
+export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,11 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Layout from '@/components/Layouts/Layout';
-import Link from 'next/link';
-import { Container } from 'reactstrap';
-import styles from '@/styles/Home.module.css';
-import { NextPage } from 'next';
+import { Counter } from '@/components/counter/Counter'
+import type { NextPage } from 'next'
 
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-        <Container className='my-5 py-5'>
-            <h1> 헬로우 타입스크립트 </h1>
-        </Container>
-    </Layout>
-  );
+    <Counter/>
+  )
 }
+
 export default Home
